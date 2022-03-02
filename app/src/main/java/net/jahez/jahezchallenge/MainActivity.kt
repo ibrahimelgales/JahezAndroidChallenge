cc6d31f8667bf71b93c29c9c61db74c14d5ba892
titanscoding <titanscoding@gmail.com>
2022-03-02 10:59:57 +0200

    add dependencies to project

--- app/src/main/java/net/jahez/jahezchallenge/MainActivity.kt
+++ app/src/main/java/net/jahez/jahezchallenge/MainActivity.kt
@@ -1,11 +1,14 @@
 package net.jahez.jahezchallenge
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+        }
     }
 }
