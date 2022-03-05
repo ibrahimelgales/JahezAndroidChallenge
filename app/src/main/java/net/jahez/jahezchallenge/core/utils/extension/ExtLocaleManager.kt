d9dd56eade7c881ea1037b53efa9e55830ee7b61
titanscoding <titanscoding@gmail.com>
2022-03-05 19:51:48 +0200

    update gradle

--- app/src/main/java/net/jahez/jahezchallenge/core/utils/extension/ExtLocaleManager.kt
+++ app/src/main/java/net/jahez/jahezchallenge/core/utils/extension/ExtLocaleManager.kt
@@ -1,34 +1,10 @@
 package net.jahez.jahezchallenge.core.utils.extension
 
-import android.app.Activity
 import android.content.Context
-import android.os.Build
-import android.os.LocaleList
 import java.util.*
 
-
-fun Activity.setLocale(languageCode: String) {
-    val overrideConfiguration = baseContext.resources.configuration
-    val context= createConfigurationContext(overrideConfiguration)
-    val resources = context.resources
-    val config = resources.configuration
-    val locale = Locale(languageCode)
-    Locale.setDefault(locale)
-    config.setLocale(locale)
-
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-        overrideConfiguration.setLocales(LocaleList(locale))
-    } else{
-        resources.updateConfiguration(config, resources.displayMetrics)
-    }
-}
-
-fun Context.initLocale(currentLanguage : String) {
-
-    val pref = getSharedPreferences(packageName, Context.MODE_PRIVATE)
+fun Context.initLocale(currentLanguage: String) {
     val res = resources
-
     // Change locale settings in the app.
     val dm = res.displayMetrics
     val conf = res.configuration
--- app/src/main/java/net/jahez/jahezchallenge/presentation/home/HomeActivity.kt
+++ app/src/main/java/net/jahez/jahezchallenge/presentation/home/HomeActivity.kt
@@ -4,16 +4,11 @@ package net.jahez.jahezchallenge.presentation.home
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.lifecycle.asLiveData
-import com.google.accompanist.insets.ProvideWindowInsets
 import dagger.hilt.android.AndroidEntryPoint
 import net.jahez.jahezchallenge.core.theme.AppDefaultTheme
 import net.jahez.jahezchallenge.core.utils.BaseComponentActivity
 import net.jahez.jahezchallenge.presentation.home.composable_screens.RestaurantsListScreen
-import net.jahez.jahezchallenge.presentation.settings.SettingsActivity
 
 @AndroidEntryPoint
 class HomeActivity : BaseComponentActivity() {
@@ -22,16 +17,14 @@ class HomeActivity : BaseComponentActivity() {
     companion object {
         fun createIntent(context: Context) = Intent(context, HomeActivity::class.java)
     }
+
     //endregion
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ProvideWindowInsets{
-                AppDefaultTheme {
-                    RestaurantsListScreen()
-                }
+            AppDefaultTheme {
+                RestaurantsListScreen()
             }
-
         }
     }
 }
